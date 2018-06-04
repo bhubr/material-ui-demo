0d75660fe051af8cdbd397cf461d2426221f53f4
Benoît Hubert <benoithubert@gmail.com>
2018-06-04 22:26:16 +0200

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+      </Grid>
